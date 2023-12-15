f5034f3aa82f45a40a41073aefc4d3dea848826b
joukey123 <0162943362@naver.com>
2023-12-15 18:58:15 +0900

    Motion - Variants
    delayChildren,
    staggerChildren,

--- src/App.tsx
+++ src/App.tsx
@@ -10,29 +10,59 @@ const Wapper = styled.div`
 `;
 
 const Box = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
   width: 200px;
   height: 200px;
   border-radius: 20px;
-  background-color: white;
+  background-color: rgba(255, 255, 255, 0.2);
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
-  start: { scale: 0 },
+const Circle = styled(motion.div)`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  background-color: white;
+  place-self: center;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVars = {
+  start: { scale: 0, opacity: 0 },
   end: {
     scale: 1,
-    rotateZ: 360,
+    opacity: 1,
     transition: {
       type: "spring",
-      delay: 1,
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.5,
     },
+  },
+};
+
+const circleVars = {
+  start: {
+    opacity: 0,
+    y: "10%",
+  },
+  end: {
+    opacity: 1,
+    y: "0%",
   },
 };
 
 function App() {
   return (
     <Wapper>
-      <Box variants={myVars} initial="start" animate="end" />
+      <Box variants={boxVars} initial="start" animate="end">
+        <Circle variants={circleVars} />
+        <Circle variants={circleVars} />
+        <Circle variants={circleVars} />
+        <Circle variants={circleVars} />
+      </Box>
     </Wapper>
   );
 }
