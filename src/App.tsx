12c15ba02645f88737f4f521287e58daa553d118
joukey123 <0162943362@naver.com>
2023-12-17 22:22:28 +0900

    drag motion
    drag motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wapper = styled.div`
   width: 100vw;
@@ -10,59 +11,49 @@ const Wapper = styled.div`
 `;
 
 const Box = styled(motion.div)`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
   width: 200px;
   height: 200px;
-  border-radius: 20px;
-  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  background-color: rgb(255, 255, 255);
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  width: 60px;
-  height: 60px;
-  border-radius: 50%;
-  background-color: white;
-  place-self: center;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const BigBox = styled(motion.div)`
+  width: 400px;
+  height: 400px;
+  border-radius: 40px;
+  background-color: rgba(255, 255, 255, 0.3);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
 `;
 
 const boxVars = {
-  start: { scale: 0, opacity: 0 },
-  end: {
-    scale: 1,
-    opacity: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.5,
-    },
+  over: {
+    scale: 1.1,
+    rotateZ: 90,
   },
-};
-
-const circleVars = {
-  start: {
-    opacity: 0,
-    y: "10%",
-  },
-  end: {
-    opacity: 1,
-    y: "0%",
+  click: {
+    scale: 0.8,
+    borderRadius: "50%",
   },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wapper>
-      <Box variants={boxVars} initial="start" animate="end">
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-      </Box>
+      <BigBox ref={biggerBoxRef}>
+        <Box
+          variants={boxVars}
+          drag
+          dragSnapToOrigin
+          dragConstraints={biggerBoxRef}
+          whileHover="over"
+          whileTap="click"
+        />
+      </BigBox>
     </Wapper>
   );
 }
