6a4f3fb72ddba826e59b32ec99e2d0ef424e8681
joukey123 <0162943362@naver.com>
2023-12-18 22:33:45 +0900

    modal event
    modal event

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 const Wapper = styled.div`
   width: 100vw;
@@ -8,61 +9,65 @@ const Wapper = styled.div`
   align-items: center;
   justify-content: center;
 `;
-
-const Box = styled(motion.div)`
+const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  width: 200px;
+  width: 50vw;
+  grid-gap: 20px;
+  grid-template-columns: repeat(3, 1fr);
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+const Box = styled(motion.div)`
   height: 200px;
   border-radius: 20px;
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
-
-const Circle = styled(motion.div)`
-  width: 60px;
-  height: 60px;
-  border-radius: 50%;
-  background-color: white;
-  place-self: center;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Overlay = styled(motion.div)`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
-
-const boxVars = {
-  start: { scale: 0, opacity: 0 },
-  end: {
-    scale: 1,
-    opacity: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.5,
-    },
-  },
-};
-
-const circleVars = {
+const overviewVar = {
   start: {
-    opacity: 0,
-    y: "10%",
+    backgroundColor: "rgba(0, 0, 0, 0)",
   },
-  end: {
-    opacity: 1,
-    y: "0%",
+  animate: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  exit: {
+    backgroundColor: "rgba(0, 0, 0, 0)",
   },
 };
-
 function App() {
+  const [id, setId] = useState<string | null>(null);
+
   return (
     <Wapper>
-      <Box variants={boxVars} initial="start" animate="end">
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-      </Box>
+      <Grid>
+        {["1", "2", "3", "4"].map((n) => (
+          <Box key={n} onClick={() => setId(n)} layoutId={n} />
+        ))}
+      </Grid>
+
+      {id && (
+        <AnimatePresence>
+          <Overlay
+            variants={overviewVar}
+            initial="start"
+            animate="animate"
+            exit="exit"
+            onClick={() => setId(null)}
+          >
+            <Box layoutId={id} style={{ width: 400, height: 200 }} />
+          </Overlay>
+        </AnimatePresence>
+      )}
     </Wapper>
   );
 }
