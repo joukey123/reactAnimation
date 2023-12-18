923c5f218fdc7c4fe526642d3e238484bf71c08a
joukey123 <0162943362@naver.com>
2023-12-18 17:55:14 +0900

    slider animation
    slider box

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 const Wapper = styled.div`
   width: 100vw;
@@ -7,62 +8,74 @@ const Wapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
   width: 200px;
   height: 200px;
   border-radius: 20px;
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 200px;
+  font-weight: bold;
+  font-size: 30px;
 `;
-
-const Circle = styled(motion.div)`
-  width: 60px;
-  height: 60px;
-  border-radius: 50%;
-  background-color: white;
-  place-self: center;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
 const boxVars = {
-  start: { scale: 0, opacity: 0 },
-  end: {
-    scale: 1,
+  entry: (isback: boolean) => ({
+    x: isback ? -500 : 500,
+    opacity: 0,
+    scale: 0,
+  }),
+  center: {
+    x: 0,
     opacity: 1,
+    scale: 1,
     transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.5,
+      duration: 0.3,
     },
   },
-};
-
-const circleVars = {
-  start: {
+  exit: (isback: boolean) => ({
+    x: isback ? 500 : -500,
     opacity: 0,
-    y: "10%",
-  },
-  end: {
-    opacity: 1,
-    y: "0%",
-  },
+    scale: 0,
+    transition: {
+      duration: 0.3,
+    },
+  }),
 };
-
 function App() {
+  const [show, setShow] = useState(1);
+  const [back, setBack] = useState(false);
+
+  const nextBtn = () => {
+    setBack(false);
+    setShow((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevBtn = () => {
+    setBack(true);
+    setShow((prev) => (prev === 1 ? 1 : prev - 1));
+  };
   return (
     <Wapper>
-      <Box variants={boxVars} initial="start" animate="end">
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-      </Box>
+      <AnimatePresence custom={back}>
+        <Box
+          custom={back}
+          variants={boxVars}
+          initial="entry"
+          animate="center"
+          exit="exit"
+          key={show}
+        >
+          {show}
+        </Box>
+      </AnimatePresence>
+      <button onClick={nextBtn}> Next </button>
+      <button onClick={prevBtn}> Prev </button>
     </Wapper>
   );
 }
